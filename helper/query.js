e73d26e642d48ead598ea1d7fed7ab52f13d549c
aaaziz2 <aumio.aziz@gmail.com>
2022-11-15 16:57:43 -0600

    Added functionality for viewing Roles, Employees, and adding department and roles

--- helper/query.js
+++ helper/query.js
@@ -1,4 +1,7 @@
 const mysql = require('mysql2');
+// https://stackoverflow.com/questions/49618069/remove-index-from-console-table
+const { Console } = require('console');
+const { Transform } = require('stream');
 require('dotenv').config()
 
 const db = mysql.createConnection(
@@ -10,6 +13,24 @@ const db = mysql.createConnection(
     }
 )
 
+function table(input) {
+    // @see https://stackoverflow.com/a/67859384
+    const ts = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+    const logger = new Console({ stdout: ts })
+    logger.table(input)
+    const table = (ts.read() || '').toString()
+    let result = '';
+    for (let row of table.split(/[\r\n]+/)) {
+      let r = row.replace(/[^┬]*┬/, '┌');
+      r = r.replace(/^├─*┼/, '├');
+      r = r.replace(/│[^│]*/, '');
+      r = r.replace(/^└─*┴/, '└');
+      r = r.replace(/'/g, ' ');
+      result += `${r}\n`;
+    }
+    console.log(result);
+  }
+  
 
 
-module.exports = { db }
+module.exports = { db, table }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer')
-const { db } = require('./helper/query')
+const { db, table } = require('./helper/query')
 
 
 const options = [
@@ -10,28 +10,147 @@ const options = [
 
 const viewAllDepartments = () => {
     db.query(`SELECT department.id AS "Department ID", department.name AS "Department" FROM department`, function (err, data) {
-      console.log('\n\n\n')
-      console.log(`All Departments:`);
-      console.table(data);
-      console.log('Press any key to return to menu')
-      console.log('\n\n\n')
+      console.log(`\n\nAll Departments:`);
+      table(data);
+      console.log('Press up or down key to return to menu')
     });
     init()
   }
 
+const viewAllRoles = () => {
+    db.query(`SELECT role.id AS "Role ID", role.title AS "Title", role.salary AS "Salary", role.department_id AS "Department ID" FROM role`, function (err, data) {
+        console.log(`\n\nAll Roles:`);
+        table(data);
+        console.log('Press up or down key to return to menu')
+    })
+    init()
+}
+
+const viewAllEmployees = () => {
+    db.query(
+        `SELECT employee.first_name AS "First", employee.last_name AS "Last", role.title AS "Title"
+        FROM employee
+        JOIN role
+        ON employee.role_id = role.id`,
+    function (err, data) {
+        console.log(`\n\nAll Employees:`);
+        table(data);
+        console.log('Press up or down key to return to menu')
+    })
+    init()
+}
+
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "department",
+                message: "What department would you like to add?"
+            }
+        ])
+        .then((data) =>{
+            db.query(`INSERT INTO department (name) VALUES ('${data.department}')`, (err,res) =>{
+                if(err) throw err
+                viewAllDepartments()
+            })             
+        })
+}
+const departmentList = ["string"]
+const getDepartments = () => {
+    while(departmentList.length>0){
+        departmentList.pop()
+    }
+    db.query("SELECT * FROM department;", function (err, res) {
+    
+        for (let i = 0; i < res.length; i++){
+            departmentList.push(`${res[i].name}`)
+        }
+    })
+    return
+}
+
+const employeeList = ["string"]
+const getEmployees = () => {
+    while(employeeList.length>0){
+        employeeList.pop()
+    }
+    db.query("SELECT * FROM employee;", function (err, res) {
+    
+        for (let i = 0; i < res.length; i++){
+            employeeList.push(`${res[i].first_name} ${res[i].last_name}`)
+        }
+    })
+    employeeList.push("N/A")
+    return
+}
+
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "department",
+                choices: departmentList,
+                message: "What departmart are you adding a role to?"
+            },
+            {
+                type: "input",
+                name: "role",
+                message: "What is the name of the role?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "What is the salary of the role?"
+            },
+        ])
+        .then((data) =>{
+            let id = departmentList.indexOf(data.department)
+            id++
+            db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${data.role}', '${data.salary}', '${id}')`, (err,res) =>{
+                if(err) throw err
+                viewAllRoles()
+            })             
+        })
+}
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "first",
+                message: "Employee's first name?"
+            },
+            {
+                type: "input",
+                name: "last",
+                message: "Employee's last name?"
+            },
+            {
+                type: "list",
+                name: "manager",
+                choices: employeeList,
+                message: "Who does this employee report to?"
+            },
+        ])
+}
 
 var questions = [
     {
         type: "list",
         message: "What would you like to do?",
         choices: options,
-        loop: false,
+        // loop: false,
         name: "selection"
     }
 ]
 
 
 const init = () => {
+    getDepartments()
+    getEmployees()
     inquirer
         .prompt(questions)
         .then((data) => {
@@ -40,19 +159,24 @@ const init = () => {
                     viewAllDepartments() 
                     break;
                 case options[1]:
+                    viewAllRoles()
                     break;
                 case options[2]:
+                    viewAllEmployees()
                     break;
                 case options[3]:
+                    addDepartment()
                     break;
                 case options[4]:
+                    addRole()
                     break;
                 case options[5]:
                     break;
                 case options[6]:
                     break;
                 default:
-                    console.log("Goodbye!")
+                    console.log('Goodbye')
+                    db.end()
                     break;
             }
         })
